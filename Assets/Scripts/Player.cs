780ae319515cfae7a16b9b43e81c467d7d7c0039
Not-Bean <alexander.bean@ontariotechu.net>
2025-10-22 16:12:43 -0400

    Applied grass & input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public GameObject TextDialogBox;
+    private float speed = 5f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,11 +15,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right
+        float vertical = Input.GetAxis("Vertical");     // W/S or Up/Down
+
+        // Movement direction
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        // Apply movement
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 
-    void Talk()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.CompareTag("NPC") && Input.GetKeyDown(KeyCode.E))
+        {
+            TextDialogBox.SetActive(true);
+            //read the specific NPC's dialog
+        }
     }
 }
