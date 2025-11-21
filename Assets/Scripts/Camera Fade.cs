5e162f85a968270758e84cf70ef511efe3bd7757
Isnt-Bean <alexander.bean@ontariotechu.com>
2025-11-21 15:56:23 -0500

    added singleton

--- Assets/Scripts/Camera Fade.cs
+++ Assets/Scripts/Camera Fade.cs
@@ -1,19 +1,46 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 
 public class CameraFade : MonoBehaviour
 {
-    
+    public bool StartFade = false;
+    public GameObject fade;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        StartFade = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (StartFade)
+        {
+            StartCoroutine(FadeIn());
+            //up the alpha value
+
+            //teleport the player to a new area
+
+            //lower the alpha value then delete using singleton
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        var temp = fade.GetComponent<Image>().color;//fade in
+        temp.a += 0.01f;
+        fade.GetComponent<Image>().color = temp;
         
+        
+        yield return new WaitForSeconds(2f);
+        
+        
+        var temp2 = fade.GetComponent<Image>().color;//fade out
+        temp2.a -= 0.01f;
+        fade.GetComponent<Image>().color = temp2;
+        StartFade = false;
+        StopCoroutine(FadeIn());
     }
 }
--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Collections;
 public class Singleton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static Singleton Instance { get; private set; }
+    public CameraFade cf;
+    public bool isTP = false;
+    void Awake()
     {
         
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
+        var temp = cf.fade.GetComponent<Image>().color;
+        if (temp.a >= 1.99f && isTP == false)
+        {
+            StartCoroutine(wait());
+            isTP = true;
+            print("Player Teleports");
+        }
+        //get the camera fade script, detect when the screen fades to black, then move the player before the fade goes away
         
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2.5f);
+        isTP = false;
     }
 }
